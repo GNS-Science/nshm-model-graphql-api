af001deb3fab0d2219cd649cf41cc7da52ae61a5
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-11-06 09:38:31 +1300

    better search indexing;

--- nshm/documents.py
+++ nshm/documents.py
@@ -1,9 +1,16 @@
 # documents.py
 
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, Index, fields
 from django_elasticsearch_dsl.registries import registry
 
 from nshm import models
+
+COMMON_INDEX = 'toshi_nshm_models_index'
+models_index = Index(COMMON_INDEX)
+models_index.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
 
 class DocumentWithNodeId(Document):
     """
@@ -16,33 +23,106 @@ class DocumentWithNodeId(Document):
         elasticseach index id (_id). If needed, this method can be overloaded
         to change this default behavior.
         """
+        #return object_instance.pk
         return f"{cls.Django.model.__name__}_{object_instance.pk}"
 
 @registry.register_document
 class SeismicHazardModelDocument(DocumentWithNodeId):
 
     class Index:
-        name = 'toshi_nshm_seismic_hazard_models' # PREFIX is important
-        # See Elasticsearch Indices API reference for available settings
-        settings = {'number_of_shards': 1,
-                    'number_of_replicas': 0}
+        name = COMMON_INDEX
             
     class Django:
         model = models.SeismicHazardModel # The model associated with this Document
         fields = [
+            "id",
             "version",
             "notes",
         ]
 
+    source_logic_tree = fields.ObjectField(properties={
+        'version': fields.TextField(),
+    })
+    gmcm_logic_tree = fields.ObjectField(properties={
+        'version': fields.TextField(),
+    })
+
+@registry.register_document
+class SourceLogicTreeDocument(DocumentWithNodeId):
+    class Index:
+        name = COMMON_INDEX
+
+    class Django:
+        model = models.SourceLogicTree
+        fields = [
+            'version',
+            'notes'
+        ]
+
+    seismic_hazard_models = fields.NestedField(properties={
+        'version': fields.TextField(),
+        'id': fields.TextField()        
+    })
+    
+    slt_weighted_components = fields.NestedField(properties={
+        'weight': fields.TextField(),
+        'id': fields.TextField()
+    })    
+
+@registry.register_document
+class SourceLogicTreeComponentDocument(DocumentWithNodeId):
+    class Index:
+        name = COMMON_INDEX
+
+    class Django:
+        model = models.SourceLogicTreeComponent
+        fields = [
+            'tag', 
+            'notes',
+            'inversion_toshi_id',
+            'background_toshi_id',
+            'tectonic_region',
+            'group'
+        ]
+
+    slt_weighted_components = fields.NestedField(properties={
+        'weight': fields.TextField(),
+        'id': fields.TextField()        
+    })
+
+
+@registry.register_document
+class SourceLogicTreeWeightedComponentDocument(DocumentWithNodeId):
+    
+    class Index:
+        name = COMMON_INDEX
+    
+    class Django:
+        model = models.SourceLogicTreeWeightedComponent
+        fields = [
+            'weight',
+        ]
+
+    source_logic_tree = fields.ObjectField(properties={
+        'version': fields.TextField(),
+        'id': fields.TextField()        
+    })
+
+    source_logic_tree_component = fields.ObjectField(properties={
+        'tag': fields.TextField(),
+        'id': fields.TextField()        
+    })
+
+
 @registry.register_document
 class HazardSolutionDocument(DocumentWithNodeId):
     class Index:
-        name = 'toshi_nshm_hazard_solutions'
-        settings = {'number_of_shards': 1,
-                    'number_of_replicas': 0}
+        name = COMMON_INDEX
+
     class Django:
         model = models.HazardSolution # The model associated with this Document
         fields = [
+            "id",
             "solution_id",
             "created",
             "vs30",
