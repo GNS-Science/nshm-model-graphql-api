e76529d32e0ffbf961d0449c01ea4edc9e37199a
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-06-05 13:09:59 +1200

    update from flask-graphql to graphql-server;

--- nshm_model_graphql_api/nshm_model_graphql_api.py
+++ nshm_model_graphql_api/nshm_model_graphql_api.py
@@ -7,7 +7,7 @@ import os
 import yaml
 from flask import Flask
 from flask_cors import CORS
-from flask_graphql import GraphQLView
+from graphql_server.flask import GraphQLView
 
 # from nshm_model_graphql_api.library_version_check import log_library_info
 from nshm_model_graphql_api.schema import schema_root
